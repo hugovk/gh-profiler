bfb68b1451f5e02a6ea2789331fe7ee2828a78cb
Eric Matthes <ehmatthes@gmail.com>
2026-05-01 17:17:25 -0400

    Remove breakpoint. Thanks Chris!

--- src/gh_profiler/utils/profile_utils.py
+++ src/gh_profiler/utils/profile_utils.py
@@ -35,7 +35,7 @@ def get_profile_info():
     cmd = f"gh api users/{pdata.username} --jq '{{login, name, created_at, company, blog, location, email, bio}}'"
     profile_info = infra_utils.run_cmd(cmd)
     pdata.profile_info = json.loads(profile_info)
-    breakpoint()
+
     if "created_at" not in pdata.profile_info:
         sys.exit(f"GitHub user '{pdata.username}' not found.")
     
